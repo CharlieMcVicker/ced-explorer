5e08bfbfe16856431943c98418a13653c846b4e1
Glitch (glitch-hello-website) <none>
2023-11-06 22:57:16 +0000

    🌨🌼 Checkpoint ./dict_verbs.js:1525279/13 ./script.js:1525279/8767 ./index.html:1525279/4037

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-import dict from "./dict_verbs.json";
+import { dict } from "./dict_verbs.js";
 
-const wordIds = Object.keys(dict);
+function hasFields(word) {
+  return (
+    word.sentence.phonetics.length > 0 &&
+    word.sentence.syllabary.length > 0 &&
+    !word.sentence.phonetics.startsWith("[")
+  );
+}
+
+const wordIds = Object.keys(dict).filter((id) => hasFields(dict[id]));
 
 function randomWord() {
   const idx = Math.floor(Math.random() * wordIds.length);
   return dict[wordIds[idx]];
 }
 
-const sentence = document.querySelector(".example-sentence"); // Get the button from the page
-sentence.innerHTML = randomWord().sentence.phonetics;
+function subAsterisk(text, pattern) {
+  var bold = /\*(.*?)\*/gm;
+  var html = text.replace(bold, pattern);
+  return html;
+}
 
+function removeAsterisk(text) {
+  return subAsterisk(text, "$1");
+}
+
+function boldAsterisk(text) {
+  return subAsterisk(text, "<strong>$1</strong>");
+}
+
+function setExampleSentence(word) {
+  sentenceSyllabary.innerHTML = boldAsterisk(word.sentence.syllabary);
+  sentencePhonetics.innerHTML = boldAsterisk(word.sentence.phonetics);
+}
+
+function nextWord() {
+  const word = randomWord();
+  setExampleSentence(word);
+}
+
+const nextBtn = document.querySelector(".next");
+nextBtn.addEventListener("click", () => nextWord());
+const sentenceSyllabary = document.querySelector(".example-syllabary"); // Get the button from the page
+const sentencePhonetics = document.querySelector(".example-phonetics"); // Get the button from the page
+
+nextWord();
